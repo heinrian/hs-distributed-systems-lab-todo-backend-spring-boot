6dd53edaac25786ff3e966470f40671af002b2ba
Andreas Heinrich <github@heinrian.com>
2022-05-20 14:16:11 +0200

    create app from initializr

---
+++ src/main/java/com/heinrian/labtodobackend/LabTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heinrian.labtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabTodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heinrian/labtodobackend/LabTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heinrian.labtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
